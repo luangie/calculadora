83a05236aa4d3678e5fb6ec8f74b5e2bde59d86d
unknown <luangie982@hotmail.com>
2021-01-28 19:58:56 -0500

    Actividad Calculadora

--- JS/javascrip.js
+++ JS/javascrip.js
@@ -1,4 +1,61 @@
 
-
 //alert("Bienvenidos");
-//swal ( "Bienvenidos")
+//swal ( "Bienvenidos");
+
+//console.log("PRUEBA HOLA MUNDO");
+
+function updateInput(newWalue, clear = false) {
+var div = document.getElementById("data");
+
+div.value = clear ? newWalue: `${div.value}${newWalue}`;
+}
+
+function printOperator(data) {
+    updateInput(` ${data.value} `);
+
+    //' ' + data.value + ''
+}
+
+function printNumber(data) {
+    updateInput(data.value);
+}
+
+function clearInput(){
+    updateInput('', true );
+}
+
+function printOperation() {
+    var data = document.getElementById('data').value;
+    var array = validateOperator(data.split(' '));
+    updateInput(array, true);
+}
+
+function validateOperator(arrayData){ 
+    var resultado;
+    var numero1,numero2;
+
+    numero1 = Number(arrayData[0]);
+    numero2 = Number(arrayData[2]);
+
+    switch (arrayData[1]) {
+        case '+':
+            resultado = numero1 + numero2;
+            break; 
+        case '-':
+            resultado = numero1 - numero2;
+            break; 
+        case '*':
+            resultado = numero1 * numero2;
+            break; 
+        case '/':
+            resultado = numero1 / numero2;
+            break; 
+
+            }
+        
+            return resultado;
+
+
+}
+
+
